b2a8da5f1a26f2deecb2254cdd408988dae5308a
Maalzadjali <Maalzadjali@unomaha.edu>
2020-04-15 20:50:25 -0600

    Json assignment done

--- foodservice/myvenv/mfscrm/crm/views.py
+++ foodservice/myvenv/mfscrm/crm/views.py
@@ -5,6 +5,19 @@ from django.shortcuts import render
 from django.db.models import Sum
 from .models import *
 from .forms import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CustomerSerializer
+
+# List at the end of the views.py
+# Lists all customers
+class CustomerList(APIView):
+
+    def get(self,request):
+        customers_json = Customer.objects.all()
+        serializer = CustomerSerializer(customers_json, many=True)
+        return Response(serializer.data)
 
 now = timezone.now()
 def home(request):
